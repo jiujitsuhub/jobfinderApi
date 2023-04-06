431025029b95adce785d8ed542a6c030f881c464
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2023-04-06 23:42:51 +0200

    initial API first setup

---
+++ src/test/java/com/jiujitsuhub/jobfinder/JobfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiujitsuhub.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
